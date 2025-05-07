10493321f3e655a0b369807914c7fa5c2a13ad6e
Stepan <reimer_stepan@mail.ru>
2025-05-07 20:43:20 +0700

    Минимальная реализация метода о предстоящих ДР

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ class BirthdayReminder:
     def get_today_birthdays(self):
         today = date.today().strftime("%m-%d")
         return [f for f in self.friends if f["birthday"][5:] == today]
+
+    def get_upcoming_birthdays(self, days=30):
+        return self.friends  # Заглушка
