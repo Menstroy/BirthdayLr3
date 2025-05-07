6f71151dab26449314587a30caee6a2e6f050374
Stepan <reimer_stepan@mail.ru>
2025-05-07 20:38:03 +0700

    Тест для метода о предстоящих ДР

--- test_birthday.py
+++ test_birthday.py
@@ -1,7 +1,7 @@
 # test_birthday.py
 import unittest
 from main import BirthdayReminder
-from datetime import date
+from datetime import date, timedelta
 class TestBirthdayReminder(unittest.TestCase):
     def test_add_friend(self):
         br = BirthdayReminder()
@@ -15,5 +15,14 @@ class TestBirthdayReminder(unittest.TestCase):
 
         birthdays = br.get_today_birthdays()
         self.assertEqual(len(birthdays), 1)
+
+
+    def test_upcoming_birthdays(self):
+        br = BirthdayReminder()
+        today = date.today()
+        future_date = (today + timedelta(days=5)).strftime("%Y-%m-%d")
+        br.add_friend("Ближайший", future_date)
+        upcoming = br.get_upcoming_birthdays(7)
+        self.assertEqual(len(upcoming), 1)
 if __name__ == '__main__':
     unittest.main()
