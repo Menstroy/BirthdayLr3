81cf16fd79e59bf4dbb5405fbbf961b1c549c114
Stepan <reimer_stepan@mail.ru>
2025-05-07 19:43:25 +0700

    Создан тест для метода добавления друга

--- test_birthday.py
+++ test_birthday.py
@@ -1,10 +1,12 @@
+# test_birthday.py
 import unittest
 from main import BirthdayReminder
 
 class TestBirthdayReminder(unittest.TestCase):
-    def test_birth(self):
-        birth = BirthdayReminder()
-        self.assertIsNotNone(birth)
+    def test_add_friend(self):
+        br = BirthdayReminder()
+        br.add_friend("Иван", "1990-05-15")
+        self.assertEqual(len(br.friends), 1)
 
 if __name__ == '__main__':
     unittest.main()
