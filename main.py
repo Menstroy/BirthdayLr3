223677d52c1aed6e790ede6d53296278a911c470
Stepan <reimer_stepan@mail.ru>
2025-05-07 20:25:02 +0700

    Минимлаьная реализация метода для проверки сегодняшних ДР

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from datetime import date
 class BirthdayReminder:
     def __init__(self):
         self.friends = []
 
     def add_friend(self, name, birthday):
         self.friends.append({"name": name, "birthday": birthday})
+
+    def get_today_birthdays(self):
+        today = date.today().strftime("%m-%d")
+        return [f for f in self.friends if f["birthday"][5:] == today]
